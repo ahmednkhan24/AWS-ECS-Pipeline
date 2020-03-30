980e37658cd0eb3a40eea63b691710d21599f7f3
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-03-29 20:57:43 -0500

    adding env

--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello world from a Node.js app!');
 });
 
+app.get('/secret', (req, res) => {
+  res.send(`mysecret: ${process.env.MYSECRET}`);
+});
+
 app.listen(3000, () => {
   console.log('Server is up on 3000');
 });
